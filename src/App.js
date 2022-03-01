d0a94b724c6536d7a5da0e10128cc191daf992bb
dwisatriow <dwisatrioow@gmail.com>
2022-03-01 16:23:36 +0700

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom";
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"; // eslint-disable-line
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("#ad343e");
@@ -11,21 +12,24 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <h2>This won&lsquo;t go away</h2>
+        <Suspense fallback={<h2>loading route ...</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
